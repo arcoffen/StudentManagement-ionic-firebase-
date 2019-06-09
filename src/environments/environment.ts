1d2ffc7483aff4b091272b5943c3182397d1b596
Arcoffen <zakariaelamim@gmail.com>
2019-06-10 00:35:23 +0100

    ionic

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
-
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCKN5H6oZWoEciq_eXb7IUcf-7Glr51siI",
+    authDomain: "myproject-bd5e3.firebaseapp.com",
+    databaseURL: "https://myproject-bd5e3.firebaseio.com",
+    projectId: "myproject-bd5e3",
+    storageBucket: "myproject-bd5e3.appspot.com",
+    messagingSenderId: "661568850414",
+    appId: "1:661568850414:web:9c14bf30e4d10890",
+  }
+ };
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
